96bd9bb29cf987f1af31f463426791425e445917
Ranjana <ranjanaranjithlal@gmail.com>
2024-07-05 00:14:57 +0530

    Fifth commit

--- ep1/gcs_upload.py
+++ ep1/gcs_upload.py
@@ -1,4 +1,4 @@
-from gcloud import storage
+from google.cloud import storage
 storage_client=storage.Client()
 bucket=storage_client.get_bucket("learning-bucket-23")
 blob=bucket.blob("tffolder/newtxt.txt")
